9e341e1c4f4c2c4ed0dd52590fa90bfb69929873
unknown <sidney.shuman95@gmail.com>
2022-03-29 10:06:18 -0400

    add test for value changes

--- app/partMonger/src/app/parts/parts/parts.component.spec.ts
+++ app/partMonger/src/app/parts/parts/parts.component.spec.ts
@@ -1,27 +1,59 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture, TestBed
+} from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { PARTSMOCK } from '../../mocks/parts-mock';
+import { PartComponent } from '../part/part.component';
 import { PartsListComponent } from '../parts-list/parts-list.component';
+import { PartService } from '../parts.service';
 import { PartsComponent } from './parts.component';
 
 describe('PartsComponent', () => {
   let component: PartsComponent;
   let fixture: ComponentFixture<PartsComponent>;
-
+  let service: any;
   beforeEach(async () => {
+    const serviceSpy = jasmine.createSpyObj('PartService', ['handleSearch']);
     await TestBed.configureTestingModule({
-      declarations: [PartsComponent, PartsListComponent],
-      imports: [HttpClientTestingModule, ReactiveFormsModule],
+      declarations: [PartsComponent, PartsListComponent, PartComponent],
+      imports: [
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+        MatCardModule,
+        RouterTestingModule,
+      ],
+      providers: [{ provide: PartService, useValue: serviceSpy }],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PartsComponent);
     component = fixture.componentInstance;
+    service = TestBed.inject(PartService);
+    service.searchParts$ = of(PARTSMOCK);
+    service.partCache$ = of([...PARTSMOCK].filter((p) => p.id === 1));
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call handleSearch', (done: DoneFn) => {
+    const input: HTMLInputElement = fixture.debugElement.query(
+      By.css('input')
+    ).nativeElement;
+    input.value = 'Grip';
+    input.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(service.handleSearch).toHaveBeenCalledOnceWith('Grip');
+      done();
+    });
   });
 });
